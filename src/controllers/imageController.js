aa679e1b4fa0c4e93a3eb111796f3e1b52f7031b
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2019-03-25 20:34:53 -0300

    Salvando imagens no banco de dados

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -2,23 +2,53 @@ const path = require('path');
 const fs = require('fs-extra');
 
 const { randomNumber } = require('../helpers/libs');
+const { Image } = require('../models');
 
 module.exports = {
   getImageId: (req, res) => {
     res.send(`Imagem número: ${req.params.id_image}`);
   },
 
-  createImage: async (req, res) => {
-    const imgUrl = randomNumber();
-    const imageTempPath = req.file.path;
-    const ext = path.extname(req.file.originalname).toLowerCase();
-    const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+  createImage: (req, res) => {
+    const saveImage = async () => {
+      const { title, description } = req.body;
+      const imgUrl = randomNumber();
 
-    if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
-      await fs.rename(imageTempPath, targetPath);
-    }
+      const images = await Image.find({ filename: imgUrl });
+      if (images.length > 0) {
+        saveImage();
+      } else {
+        const imageTempPath = req.file.path;
+        const ext = path.extname(req.file.originalname).toLowerCase();
+        const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
 
-    res.send('Recebido');
+        if (
+          ext === '.png' ||
+          ext === '.jpg' ||
+          ext === '.jpeg' ||
+          ext === '.gif'
+        ) {
+          await fs.rename(imageTempPath, targetPath);
+
+          const newImage = new Image({
+            title,
+            filename: imgUrl + ext,
+            description
+          });
+
+          const imageSaved = await newImage.save();
+          // res.redirect('/images');
+          res.send('Concluido!');
+        } else {
+          await fs.unlink(imageTempPath);
+          res
+            .status(500)
+            .json({ error: 'Somente arquivos de imagens são permitidos!' });
+        }
+      }
+    };
+
+    saveImage();
   },
 
   likeImage: (req, res) => {
